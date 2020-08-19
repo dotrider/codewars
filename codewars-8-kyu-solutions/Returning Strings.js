51f5f3a2be0f0f76a71b0dc6ee8b8214ffd70a81
dotrider <56666461+dotrider@users.noreply.github.com>
2020-08-18 19:01:38 -0700

    Returning Strings 8 kyu

---
+++ codewars-8-kyu-solutions/Returning Strings.js
@@ -0,0 +1,13 @@
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+// FUNDAMENTALS STRINGS
+
+
+
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`;
+  }
