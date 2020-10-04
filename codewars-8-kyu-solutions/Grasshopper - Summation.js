26a0a059639f97f6206d2870db7f844671cf3d1f
dotrider <56666461+dotrider@users.noreply.github.com>
2020-10-03 19:02:46 -0700

    Grasshopper - Summation 8kyu

---
+++ codewars-8-kyu-solutions/Grasshopper - Summation.js
@@ -0,0 +1,22 @@
+
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+// For example:
+
+// summation(2) -> 3
+// 1 + 2
+
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+// FUNDAMENTALS LOOPS CONTROL FLOW BASIC LANGUAGE FEATURES
+
+
+var summation = function (num) {
+ 
+    if (num === 1){
+      return num; 
+    }
+  
+    return num + summation(num-1);
+    
+  }
