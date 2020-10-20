ca9e4c68e780b2e96e8e6aa366dd1d7a42400066
dotrider <56666461+dotrider@users.noreply.github.com>
2020-10-19 23:33:50 -0700

    You Cant Code Under Pressure #1

---
+++ codewars-8-kyu-solutions/You Can't Code Under Pressure #1.js
@@ -0,0 +1,9 @@
+
+// Code as fast as you can! You need to double the integer and return it.
+
+// FUNDAMENTALS NUMBERS INTEGERS
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+  }
