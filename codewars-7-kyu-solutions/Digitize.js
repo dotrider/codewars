e225928238fc37f3e80a29fb6038afeaaaf2df12
dotrider <dotrider883@gmail.com>
2021-04-10 23:30:46 -0700

    Digitize 7kyu

---
+++ codewars-7-kyu-solutions/Digitize.js
@@ -0,0 +1,18 @@
+
+// Given a non-negative integer, return an array / a list of the individual digits in order.
+
+// Examples:
+
+// 123 => [1,2,3]
+
+// 1 => [1]
+
+// 8675309 => [8,6,7,5,3,0,9]
+
+// ALGORITHMS LISTS DATA STRUCTURES NUMBERS PARSING STRINGS
+
+
+function digitize(n) {
+
+    return n.toString().split('').map(n => +n)
+  }
