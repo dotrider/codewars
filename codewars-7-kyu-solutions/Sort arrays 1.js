3a12508b0cb1194fb014253ed33fcc6023ad12d3
dotrider <dotrider883@gmail.com>
2021-05-04 21:23:01 -0700

    Sort arrays -1 7kyu

---
+++ codewars-7-kyu-solutions/Sort arrays 1.js
@@ -0,0 +1,10 @@
+
+// Just a simple sorting usage. Create a function that returns the elements of the input-array in a sorted manner.
+
+// FUNDAMENTALS SORTING ALGORITHMS ARRAYS
+
+// input: names - unsorted array
+// output: sorted array
+sortme = function( names ){
+    return names.sort()
+  }
