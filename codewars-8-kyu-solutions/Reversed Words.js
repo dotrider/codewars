3e4fd6433d604d64b2b982f0b99a61430c1faf08
pablo <dotrider883@gmail.com>
2021-06-22 21:05:26 -0700

    Reversed Words 8kyu

---
+++ codewars-8-kyu-solutions/Reversed Words.js
@@ -0,0 +1,12 @@
+
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+// Example:
+
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+// ALGORITHMS STRINGS
+
+function reverseWords(str){
+    return str.split(' ').reverse().join(' ')
+  }
