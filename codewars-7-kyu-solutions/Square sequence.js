520a71c7046e6aac25a66b2ae967598da61911e3
pablo <dotrider883@gmail.com>
2022-10-26 23:24:26 -0700

    Square sequence 7kyu

---
+++ codewars-7-kyu-solutions/Square sequence.js
@@ -0,0 +1,16 @@
+// Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. If n is negative or zero, return an empty array/list.
+
+// Examples
+// 2, 5  -->  [2, 4, 16, 256, 65536]
+// 3, 3  -->  [3, 9, 81]
+
+// MATHEMATICS FUNDAMENTALS
+
+function squares(x, n) {
+  let arr = [];
+  for (let i = 0; i < n; i++) {
+    arr.push(x);
+    x *= x;
+  }
+  return arr;
+}
